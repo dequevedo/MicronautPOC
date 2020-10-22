3368de0e05db9a9ab255bec335053d1080f58694
Daniel Quevedo <daniel.quevedo@sensedia.com>
2020-10-22 15:22:06 -0300

    Post and Get

--- src/main/java/com/mocronautpoc/MainController.java
+++ src/main/java/com/mocronautpoc/MainController.java
@@ -3,13 +3,19 @@ package com.mocronautpoc;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Produces;
 
-@Controller("/hello")
+@Controller("/microservice/")
 public class MainController {
     @Get
     @Produces(MediaType.APPLICATION_JSON)
-    public Person index() {
+    public Person getPerson() {
         return new Person("Daniel", 22, "Male");
+    }
+
+    @Post("/person")
+    public Person postPerson(Person person) {
+        return person;
     }
 }
